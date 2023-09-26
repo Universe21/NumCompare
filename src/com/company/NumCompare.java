17942eac6b2d6f689c1fb84c49d5e41982233226
Universe21 <leachhs@mail.uc.edu>
2023-09-25 21:15:33 -0400

    Projectit

---
+++ src/com/company/NumCompare.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NumCompare {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int userInputOne, userInputTwo;
+
+        {
+            System.out.println("Please enter first Integer Number to be compared:");
+            userInputOne = sc.nextInt();
+            System.out.println("Please enter second Integer Number to be compered:");
+            userInputTwo = sc.nextInt();
+            if (userInputOne < userInputTwo) {
+
+                System.out.println("Input two greater than input one.");
+
+            } else if (userInputOne > userInputTwo) {
+
+                System.out.println("Input one greater than input two.");
+
+            } else {
+
+                System.out.println("Both the integer numbers are the same.");
+            }
+
+        }  {
+            System.out.println("Wrong input. Please try again.");
+            //flag = false;
+        }
+
+
+    }
+
+
+}
